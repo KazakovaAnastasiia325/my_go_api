3cc108ef4a88a01c181efda98129f952c367df19
Kazakova Anastasiia <130105.kaz@gmail.com>
2026-05-20 00:16:34 +0500

    Обновление

--- main.go
+++ main.go
@@ -3,21 +3,32 @@ package main
 import (
 	"fmt"
 	"my_api/database"
-	"my_api/handlers"
+
 	"net/http"
-	
+	"os"
+	"log"
+	"github.com/joho/godotenv"
+	"my_api/routes"
 )
 func main() {
-	connStr := "postgres://postgres:0000@localhost:5432/user_db"
-	database.InitDB(connStr)
-	defer database.DB.Close()
-	http.HandleFunc("/register", handlers.RegisterUserHandler)
-	http.HandleFunc("/authenticate", handlers.AuthenticateUserHandler)
-	http.HandleFunc("/welcome", handlers.WelcomeHandler)
-	fmt.Println("Сервер запущен на порту 8080...")
-	if err := http.ListenAndServe(":8080", nil); err != nil {
-		fmt.Printf("Ошибка при запуске сервера: %v\n", err)
+	err := godotenv.Load()
+	if err != nil {
+		log.Println("Предупреждение: файл .env не найден")
 	}
+	connStr := os.Getenv("CONN_STR")
+	if connStr == "" {
+		log.Fatal("Переменная окружения CONN_STR не установлена")
+		return
+	}
+    database.InitDB(connStr)
+    defer database.DB.Close()
+
+    mux := routes.SetupRoutes()
+	handlerWithCORS := routes.EnableCORS(mux)
+    fmt.Println("Сервер запущен на http://localhost:8080")
+    if err := http.ListenAndServe(":8080", handlerWithCORS); err != nil {
+        log.Fatal(err)
+    }
 
 }
 
