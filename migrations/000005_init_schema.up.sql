6e008f8efaf95cfe03b4ba56378e75e41c4e486d
Kazakova Anastasiia <130105.kaz@gmail.com>
2026-05-26 15:43:42 +0500

    добавление таблиц уведомлений и статусов

---
+++ migrations/000005_init_schema.up.sql
@@ -0,0 +1,17 @@
+-- Таблица заказов
+CREATE TABLE orders (
+    id SERIAL PRIMARY KEY,
+    user_id INT,
+    total_price INT,
+    status VARCHAR(50) DEFAULT 'Оплачен', -- Оплачен, Собран, В пути, Доставлен
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+-- Таблица уведомлений
+CREATE TABLE notifications (
+    id SERIAL PRIMARY KEY,
+    user_id INT,
+    message TEXT,
+    is_read BOOLEAN DEFAULT FALSE,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
