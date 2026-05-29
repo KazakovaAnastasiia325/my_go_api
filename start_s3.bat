e71f9e8cafcd79163341fafbf5187e749ff90fb7
Kazakova Anastasiia <130105.kaz@gmail.com>
2026-05-29 12:35:09 +0500

    батник для запуска проекта

--- start_s3.bat
+++ start_s3.bat
@@ -1,3 +1,16 @@
 @echo off
-rclone serve s3 C:\ProjectData\ --addr :9000
+:: Запуск Rclone (если он теперь лежит в папке с проектом)
+echo Запуск Rclone...
+start "Rclone"  rclone serve s3 C:\Users\Настя\Desktop\2\ --addr :9000
+
+:: Запуск бэкенда Go
+echo Запуск Go сервера...
+start "Go Backend" go run main.go
+
+:: Запуск фронтенда
+echo Запуск React...
+cd frontend
+start "React Frontend" npm run dev
+
+echo Все сервисы запущены!
 pause
